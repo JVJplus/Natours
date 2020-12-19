30e8f7d043e200e073df7b2c34b321ab66a7f661
Jay Prakash Mahto <jvjplus@gmail.com>
2020-12-19 07:56:57 +0530

    eslint issues fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
+
 dotenv.config({
   path: './config.env'
-})
+});
 // console.log(process.env);
 
-const app=require('./app');
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening to port: ${port}...`);
 });
