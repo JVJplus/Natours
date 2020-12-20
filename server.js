973fe925ddee9f90c81b5d140050f54353f92a84
Jay Prakash Mahto <jvjplus@gmail.com>
2020-12-20 08:54:53 +0530

    test model created

--- server.js
+++ server.js
@@ -26,6 +26,39 @@ try {
   console.log(err);
 }
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A tour must have a name']
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  rating: {
+    type: Number,
+    defaultValue: 4.5
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The world of JVJplus',
+  price: 95653,
+  rating: 5
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR: ', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}...`);
